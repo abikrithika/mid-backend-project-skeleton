d5e5c899230a6f4c35e3763f289be4f01d239bdd
Abi <abikrithika@gmail.com>
2026-05-14 15:59:51 +0200

    docs: add swagger annotations for auth routes

--- api/src/routers/auth.js
+++ api/src/routers/auth.js
@@ -4,6 +4,52 @@ import { authenticateToken } from "#middlewares/auth.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
+
 router.post("/signup", signup);
 
 router.post("/login", login);
